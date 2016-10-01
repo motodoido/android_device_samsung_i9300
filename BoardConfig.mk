c4e51b6a623106580a14bc32a205f2afdb2cc9d1
Simon Shields <keepcalm444@gmail.com>
2016-10-01 22:11:20 +1000

    i9300: switch to mainline kernel
    Change-Id: Id91c8734341fbcceb24a56329695b224b2a1f8d9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
+TARGET_KERNEL_SOURCE := kernel/samsung/i9300
 TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
 
 # Recovery
